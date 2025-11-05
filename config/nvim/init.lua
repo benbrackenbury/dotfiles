60a19214a38335f154990d6496fccd47120e2374
Ben Brackenbury <benbrack22@icloud.com>
2025-11-05 21:57:20 +0000

    style(nvim): more transparency

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,6 +24,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.o.exrc = true
 vim.o.secure = true
+vim.o.laststatus=2
 
 vim.diagnostic.config({
     virtual_text = false,
@@ -135,9 +136,24 @@ vim.api.nvim_create_autocmd({ "WinEnter", "WinLeave" }, {
 })
 vim.wo.cursorline = true
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "TelescopePrompt",
+    callback = function()
+        vim.wo.cursorline = false
+    end,
+})
+
 dofile(vim.fn.expand('~') .. "/dotfiles/themes/current/neovim.lua")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, 'LineNrNC', { fg = '#404040' })
+vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#808080' })
